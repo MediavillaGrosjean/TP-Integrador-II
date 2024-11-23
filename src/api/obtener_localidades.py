c6cef32a8a49e540d170626b17b4974ba54f7df6
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-11-22 23:46:21 -0300

    Agregar mejora en la obtención de localidades

--- src/api/obtener_localidades.py
+++ src/api/obtener_localidades.py
@@ -1,8 +1,15 @@
 import requests
 from requests.exceptions import RequestException
 from logger import manejar_excepcion, registrar_actividad
+from db import consultar_documentos
 
 def obtener_localidades(provincia_id):
+    localidades_guardadas = consultar_documentos("localidades", {"provincia_id": provincia_id})
+
+    if localidades_guardadas:
+        registrar_actividad(f"Las localidades de la provincia {provincia_id} ya están guardadas.")
+        return localidades_guardadas
+
     url = f"https://apis.datos.gob.ar/georef/api/municipios?provincia={provincia_id}&max=5000"
 
     try:
