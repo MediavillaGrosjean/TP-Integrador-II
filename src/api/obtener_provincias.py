dba455cc8472bb38eef82297ef260085cf970a70
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-11-22 23:34:55 -0300

    Agregar mejora en la obtención de provincias

--- src/api/obtener_provincias.py
+++ src/api/obtener_provincias.py
@@ -1,9 +1,17 @@
 import requests
 from requests.exceptions import RequestException
 from logger import manejar_excepcion, registrar_actividad
+from db import consultar_documentos
 
 def obtener_provincias():
+    provincias_guardadas = consultar_documentos("provincias")
+
+    if provincias_guardadas:
+        registrar_actividad(f"Las provincias ya están guardadas en la base de datos.")
+        return provincias_guardadas
+
     url = "https://apis.datos.gob.ar/georef/api/provincias"
+
     try:
         response = requests.get(url)
         response.raise_for_status()
